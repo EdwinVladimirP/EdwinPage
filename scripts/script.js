8867d2f31015a55ada54144c97f89b8651876ecb
EdwinVladimirP <edwinmontV87@hotmail.com>
2022-12-15 15:20:32 +0100

    15:20

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const scrollToTop = document.querySelector(".scrollToTop");
+scrollToTop.addEventListener("click", ()=> {
+    window.scrollTo({ top: 0}); 
+});
+window.addEventListener("scroll", ()=> {
+    window.pageYOffset > 100
+     ? (scrollToTop.style.display= "block") 
+     : (scrollToTop.style.display= "none");
+});
